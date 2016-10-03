3995409cf843c432aff6b5c4b62d8e66773e4cfd
Mike Treaster <treaster@gmail.com>
2016-10-03 09:21:22 -0700

    Initial add of go_shattr code.

---
+++ attrs_test.go
@@ -0,0 +1,16 @@
+package shattr
+
+import "testing"
+
+import "bytes"
+import "fmt"
+
+func TestShellAttributes(t *testing.T) {
+  var buffer bytes.Buffer
+
+  output := NewWriter(&buffer, Underline, Blue)
+  output.Write([]byte("testing"))
+  if buffer.String() != fmt.Sprintf("%c[0;4;34mtesting%c[0m", 27, 27) {
+    t.Errorf("got wrong output somehow")
+  }
+}
